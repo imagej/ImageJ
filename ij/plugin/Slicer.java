07bc49c3903ea014914cd4899ab352c1e7023426
Wayne Rasband <wsr@nih.gov>
2009-04-20 22:37:05 +0200

    updated 20.04.2009 22:37

--- ij/plugin/Slicer.java
+++ ij/plugin/Slicer.java
@@ -44,46 +44,45 @@ public class Slicer implements PlugIn, TextListener, ItemListener {
 	private double[] dy;
 
 	public void run(String arg) {
-		 imp = WindowManager.getCurrentImage();
-		 if (imp==null) {
-				IJ.noImage();
-				return;
-		 }
-		 int stackSize = imp.getStackSize();
-		 Roi roi = imp.getRoi();
-		 int roiType = roi!=null?roi.getType():0;
-		 // stack required except for ROI = none or RECT
-		 if (stackSize<2 && roi!=null && roiType!=Roi.RECTANGLE) {
-				IJ.error("Reslice...", "Stack required");
-				return;
-		 }
-		 if (imp.isHyperStack()) {
-				IJ.error("Reslice...", "This command does not currently work with hyperstacks");
-				return;
-		 }
-		 // permissible ROI types: none,RECT,*LINE
-		 if (roi!=null && roiType!=Roi.RECTANGLE && roiType!=Roi.LINE && roiType!=Roi.POLYLINE && roiType!=Roi.FREELINE) {
-				IJ.error("Reslice...", "Line or rectangular selection required");
-				return;
-		 }
-		 if (!showDialog(imp))
-				return;
-		 long startTime = System.currentTimeMillis();
-		 ImagePlus imp2 = null;
-		 rgb = imp.getType()==ImagePlus.COLOR_RGB;
-		 notFloat = !rgb && imp.getType()!=ImagePlus.GRAY32;
-		 imp2 = reslice(imp);
-		 if (imp2==null)
-				return;
-		 ImageProcessor ip = imp.getProcessor();
-		 double min = ip.getMin();
-		 double max = ip.getMax();
-		 if (!rgb) imp2.getProcessor().setMinAndMax(min, max);
-		 imp2.show();
-		 if (noRoi)
-				imp.killRoi();
+		imp = WindowManager.getCurrentImage();
+		if (imp==null) {
+			IJ.noImage();
+			return;
+		}
+		int stackSize = imp.getStackSize();
+		Roi roi = imp.getRoi();
+		int roiType = roi!=null?roi.getType():0;
+		// stack required except for ROI = none or RECT
+		if (stackSize<2 && roi!=null && roiType!=Roi.RECTANGLE) {
+			IJ.error("Reslice...", "Stack required");
+			return;
+		}
+		// permissible ROI types: none,RECT,*LINE
+		if (roi!=null && roiType!=Roi.RECTANGLE && roiType!=Roi.LINE && roiType!=Roi.POLYLINE && roiType!=Roi.FREELINE) {
+			IJ.error("Reslice...", "Line or rectangular selection required");
+			return;
+		}
+		if (!showDialog(imp))
+			return;
+		long startTime = System.currentTimeMillis();
+		ImagePlus imp2 = null;
+		rgb = imp.getType()==ImagePlus.COLOR_RGB;
+		notFloat = !rgb && imp.getType()!=ImagePlus.GRAY32;
+		if (imp.isHyperStack())
+			imp2 = resliceHyperstack(imp);
 		else
-				imp.draw();
+			imp2 = reslice(imp);
+		if (imp2==null)
+			return;
+		ImageProcessor ip = imp.getProcessor();
+		double min = ip.getMin();
+		double max = ip.getMax();
+		if (!rgb) imp2.getProcessor().setMinAndMax(min, max);
+		imp2.show();
+		if (noRoi)
+			imp.killRoi();
+		else
+			imp.draw();
 		IJ.showStatus(IJ.d2s(((System.currentTimeMillis()-startTime)/1000.0),2)+" seconds");
 	}
 
@@ -154,6 +153,54 @@ public class Slicer implements PlugIn, TextListener, ItemListener {
 				cal.pixelHeight = tmp;
 		 }
 		 return imp2;
+	}
+
+	ImagePlus resliceHyperstack(ImagePlus imp) {
+		int channels = imp.getNChannels();
+		int slices = imp.getNSlices();
+		int frames = imp.getNFrames();
+		if (slices==1) {
+			IJ.error("Reslice...", "Cannot reslice z=1 hyperstacks");
+			return null;
+		}
+		int c1 = imp.getChannel();
+		int z1 = imp.getSlice();
+		int t1 = imp.getFrame();
+		int width = imp.getWidth();
+		int height = imp.getHeight();
+		ImagePlus imp2 = null;
+		ImageStack stack2 = null;
+		Roi roi = imp.getRoi();
+		for (int t=1; t<=frames; t++) {
+			for (int c=1; c<=channels; c++) {
+				ImageStack tmp1Stack = new ImageStack(width, height);
+				for (int z=1; z<=slices; z++) {
+					imp.setPositionWithoutUpdate(c, z, t);
+					tmp1Stack.addSlice(null, imp.getProcessor());
+				}
+				ImagePlus tmp1 = new ImagePlus("tmp", tmp1Stack);
+				tmp1.setCalibration(imp.getCalibration());
+				tmp1.setRoi(roi);
+				ImagePlus tmp2 = reslice(tmp1);
+				int slices2 = tmp2.getStackSize();
+				if (imp2==null) {
+					imp2 = tmp2.createHyperStack("Reslice of "+imp.getTitle(), channels, slices2, frames, tmp2.getBitDepth());
+					stack2 = imp2.getStack();
+				}
+				ImageStack tmp2Stack = tmp2.getStack();
+				for (int z=1; z<=slices2; z++) {
+					imp.setPositionWithoutUpdate(c, z, t);
+					int n2 = imp2.getStackIndex(c, z, t);
+					stack2.setPixels(tmp2Stack.getPixels(z), n2);
+				}
+			}
+		}
+		imp.setPosition(c1, z1, t1);
+		if (channels>1 && imp.isComposite()) {
+			imp2 = new CompositeImage(imp2, ((CompositeImage)imp).getMode());
+			((CompositeImage)imp2).copyLuts(imp);
+		}
+		return imp2;
 	}
 
 	boolean showDialog(ImagePlus imp) {
