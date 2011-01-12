aa04d25b2d1b0cb9c3983d2b4cf0ac7257cbdb9e
Wayne Rasband <wsr@nih.gov>
2011-01-12 11:20:08 -0500

    2011.01.12 (1.44n7)

--- ij/macro/Functions.java
+++ ij/macro/Functions.java
@@ -3550,6 +3550,8 @@ public class Functions implements MacroConstants, Measurements {
 			ImageProcessor.setUseBicubic(state);
 		else if (arg1.startsWith("wand")||arg1.indexOf("points")!=-1)
 			Wand.setAllPoints(state);
+		else if (arg1.startsWith("loop"))
+			Calibration.setLoopBackAndForth(state);
 		else
 			interp.error("Invalid option");
 	}
