940289b1a37a8fae2a6b006ff418667557b37108
Wayne Rasband <wsr@nih.gov>
2013-02-18 00:29:04 -0600

    * 1.47k 17 February 2013       + Thanks to Norbert Vischer, fixed a 1.47j regression that caused         the Analyze>Plot Profile command to not work as expected with         line selections wider than 1.

--- ij/ImageJ.java
+++ ij/ImageJ.java
@@ -73,7 +73,7 @@ public class ImageJ extends Frame implements ActionListener,
 	MouseListener, KeyListener, WindowListener, ItemListener, Runnable {
 
 	/** Plugins should call IJ.getVersion() or IJ.getFullVersion() to get the version string. */
-	public static final String VERSION = "1.47j";
+	public static final String VERSION = "1.47k";
 	public static final String BUILD = ""; 
 	public static Color backgroundColor = new Color(220,220,220); //224,226,235
 	/** SansSerif, 12-point, plain font. */
