420981d66d2461794fd15b85c118535f0333a4fd
aschain <aaronssjunk@aol.com>
2013-09-25 12:57:57 -0400

    Added StartupMacros.ijm as a StartupMacro to be installed before StartupMacros.fiji.ijm if present

--- ij/gui/Toolbar.java
+++ ij/gui/Toolbar.java
@@ -1310,6 +1310,12 @@ public class Toolbar extends Canvas implements MouseListener, MouseMotionListene
 		String path = IJ.getDirectory("macros")+"StartupMacros.txt";
 		File f = new File(path);
 		if (!f.exists()) {
+			String path2 = IJ.getDirectory("macros")+"StartupMacros.ijm";
+			f = new File(path2);
+			if (f.exists())
+				path = path2;
+		}
+		if (!f.exists()) {
 			String path2 = IJ.getDirectory("macros")+"StartupMacros.fiji.ijm";
 			f = new File(path2);
 			if (!f.exists()) {
