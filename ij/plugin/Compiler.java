c8bc58cfac3d258b079530f4d316e388af7b6c28
Wayne Rasband <rasband@gmail.com>
2014-05-24 11:52:48 -0400

    2014.05.24 (1.49b9)

--- ij/plugin/Compiler.java
+++ ij/plugin/Compiler.java
@@ -193,7 +193,7 @@ public class Compiler implements PlugIn, FilenameFilter {
 			File f2 = new File(path+list[i]);
 			if (f2.isDirectory())
 				addJars(path+list[i], sb);
-			else if (list[i].endsWith(".jar")&&(list[i].indexOf("_")==-1||list[i].equals("loci_tools.jar"))) {
+			else if (list[i].endsWith(".jar")&&(list[i].indexOf("_")==-1||list[i].equals("loci_tools.jar")||list[i].equals("ImageJ_3D_Viewer.jar"))) {
 				sb.append(File.pathSeparator+path+list[i]);
 				if (IJ.debugMode) IJ.log("javac classpath: "+path+list[i]);
 			}
