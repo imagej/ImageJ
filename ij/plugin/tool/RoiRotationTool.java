a7c29eaa106615a53698542ec5f2d6cd0d70dafc
Wayne Rasband <rasband@gmail.com>
2015-10-24 16:41:58 -0400

    2015.10.24 (1.50d26)

--- ij/plugin/tool/RoiRotationTool.java
+++ ij/plugin/tool/RoiRotationTool.java
@@ -9,24 +9,46 @@ package ij.plugin.tool;
 import ij.*;
 import ij.gui.*;
 import ij.plugin.RoiRotator;
+import ij.plugin.tool.PlugInTool;
+import java.awt.Rectangle;
 import java.awt.event.*;
+
 
 public class RoiRotationTool extends PlugInTool {
 	ImageCanvas ic = null;
 	int startX=0, startY=0;
+	Roi roi, newRoi;
+	int centerX, centerY, xNew, yNew, dx1, dy1, dx2, dy2;
+	double l, l1, l2, dy, phi, phi1, phi2;
+	
+	static final int UPDOWNROTATION=0, CIRCULARROTATION=1;
+	int defaultRotationMode = CIRCULARROTATION;
 	
 	public void mousePressed(ImagePlus imp, MouseEvent e) {
+		if (imp == null) return;
 		ic = imp.getCanvas();
-		if (imp == null || ic == null)
-			return;
-		Roi roi = imp.getRoi();
+		if (ic == null) return;
+		roi = imp.getRoi();
 		if (roi==null) {
 			IJ.beep();
 			IJ.showStatus("No selection");
 			return;
 		}
+		
+		startX = ic.offScreenX(e.getX());
+		startY = ic.offScreenY(e.getY());
+		
+		if (defaultRotationMode == UPDOWNROTATION){
+			centerX = imp.getWidth()/2;
+			centerY = imp.getHeight()/2;
+		}
+		else{
+			Rectangle bounds = roi.getBounds();
+			centerX = bounds.x + bounds.width/2;
+			centerY = bounds.y + bounds.height/2;
+		}
 	}
-
+	
 	public void mouseDragged(ImagePlus imp, MouseEvent e) {
 		rotateRoi(imp, e.getX(), e.getY());
 	}
@@ -38,51 +60,53 @@ public class RoiRotationTool extends PlugInTool {
 	public String getToolName() {
 		return "Selection Rotator";
 	}
-
+	
 	public String getToolIcon() {
 		return "C037D06D15D16D24D25D26D27D28D29D2aD33D34D35D36D37D3bD3cD42D43D44D45D46D47D48D4cD4dDb1Db2Db6Db7Db8Db9DbaDbbDbcDc2Dc3Dc7Dc8Dc9DcaDcbDd4Dd5Dd6Dd7Dd8Dd9DdaDe8De9Df8CabcD05D14D17D18D19D1aD23D2bD2cD32D3dD41D51D52D53D54D55D56D57D58Da6Da7Da8Da9DaaDabDacDadDbdDc1DccDd2Dd3DdbDe4De5De6De7DeaDf9";
 	}
 
-	void rotateRoi(ImagePlus imp, int sx, int sy) {
-		if (imp == null || ic == null)
-			return;
-		Roi roi = imp.getRoi();
-		if (roi == null)
-			return;
+	void rotateRoi(ImagePlus imp, int sx, int sy){
+		if (imp == null || ic == null) return;
+		
+		roi = imp.getRoi();
+		if (roi == null) return;
 		boolean imageRoi = roi instanceof ImageRoi;
 		if (imageRoi)
 			((ImageRoi)roi).setZeroTransparent(true);
 		
 		// Mouse coordinate handling
-		int centerX = imp.getWidth()/2;
-		int centerY = imp.getHeight()/2;
+		xNew = ic.offScreenX(sx);
+		yNew = ic.offScreenY(sy);
 		
-		int xNew = ic.offScreenX(sx);
-		int yNew = ic.offScreenY(sy);
-		
-		if (startX == 0 && startY == 0) {
-			startX = xNew;
+		if (startX == 0 && startY == 0){
+			startX = xNew;	
 			startY = yNew;
 			return;
 		}
 		
-		int dx1 = centerX - xNew;
-		int dy1 = centerY - yNew;
-		int dx2 = centerX - startX;
-		int dy2 = centerY - startY;
+		dx1 = centerX - xNew;
+		dy1 = centerY - yNew;
+		dx2 = centerX - startX;	
+		dy2 = centerY - startY;
 		
-		double l1 = Math.sqrt(dx1*dx1 + dy1*dy1);
-		double l2 = Math.sqrt(dx2*dx2 + dy2*dy2);
-		double l = (l1 + l2)/2.0;
-		double dy3 = yNew - startY;
+		l1 = Math.sqrt(dx1*dx1 + dy1*dy1);
+		l2 = Math.sqrt(dx2*dx2 + dy2*dy2);
+		l = (l1 + l2)/2.0;
+		dy = yNew - startY;
 		
-		if (l==0 || dy3==0)
-			return;
-		startX = xNew;
+		if (l==0 || dy==0)  return;
+		startX = xNew;	
 		startY = yNew;
+				
+		if (defaultRotationMode == UPDOWNROTATION)
+			phi = Math.atan2(dy, l);
+		else{
+			phi1 = Math.atan2(dy1, dx1);	
+			phi2 = Math.atan2(dy2, dx2);
+			phi = phi1 - phi2;
+		}
 		
-		double angle = Math.atan2(dy3, l);				
-		Roi newRoi = RoiRotator.rotate(roi, angle*180/Math.PI);
+		newRoi = RoiRotator.rotate(roi, phi*180/Math.PI);
 		if (imageRoi)
 			imp.draw();
 		else
@@ -90,4 +114,3 @@ public class RoiRotationTool extends PlugInTool {
 	}
 
 }
-
