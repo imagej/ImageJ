083e3be737cb1e329f075c037521da2af1dc985c
Wayne Rasband <rasband@gmail.com>
2018-03-22 12:24:13 -0400

    2018.03.22 (1.52a11)

--- ij/plugin/SubHyperstackMaker.java
+++ ij/plugin/SubHyperstackMaker.java
@@ -15,9 +15,9 @@ import java.awt.Color;
  */
 public class SubHyperstackMaker implements PlugIn {
 
-	public void run(final String arg) {
+	public void run(String arg) {
 		// verify input image is appropriate
-		final ImagePlus input = WindowManager.getCurrentImage();
+		ImagePlus input = WindowManager.getCurrentImage();
 		if (input == null) {
 			IJ.showMessage("No image open.");
 			return;
@@ -26,15 +26,15 @@ public class SubHyperstackMaker implements PlugIn {
 			IJ.showMessage("Image is not a stack.");
 			return;
 		}
-		final int cCount = input.getNChannels();
-		final int zCount = input.getNSlices();
-		final int tCount = input.getNFrames();
-		final boolean hasC = cCount > 1;
-		final boolean hasZ = zCount > 1;
-		final boolean hasT = tCount > 1;
+		int cCount = input.getNChannels();
+		int zCount = input.getNSlices();
+		int tCount = input.getNFrames();
+		boolean hasC = cCount > 1;
+		boolean hasZ = zCount > 1;
+		boolean hasT = tCount > 1;
 
 		// prompt for C, Z and T ranges
-		final GenericDialog gd = new GenericDialog("Subhyperstack Maker");
+		GenericDialog gd = new GenericDialog("Subhyperstack Maker");
 		gd.addMessage("Enter a range (e.g. 2-14), a range with increment\n"
 			+ "(e.g. 1-100-2) or a list (e.g. 7,9,25,27)", null, Color.darkGray);
 		if (hasC) gd.addStringField("Channels:", "1-" + cCount, 40);
@@ -42,160 +42,138 @@ public class SubHyperstackMaker implements PlugIn {
 		if (hasT) gd.addStringField("Frames:", "1-" + tCount, 40);
 		gd.showDialog();
 		if (gd.wasCanceled()) return;
-		final String cString = hasC ? gd.getNextString() : "1";
-		final String zString = hasZ ? gd.getNextString() : "1";
-		final String tString = hasT ? gd.getNextString() : "1";
+		String cString = hasC ? gd.getNextString() : "1";
+		String zString = hasZ ? gd.getNextString() : "1";
+		String tString = hasT ? gd.getNextString() : "1";
 
 		// compute subhyperstack
-		final ImagePlus output =
-			makeSubhyperstack(input, cString, zString, tString);
+		ImagePlus output = makeSubhyperstack(input, cString, zString, tString);
 
 		// display result
 		output.show();
 	}
 
-	public static ImagePlus makeSubhyperstack(final ImagePlus input,
-		final String cString, final String zString, final String tString)
-	{
-		final ArrayList<Integer> cList = parseList(cString, input.getNChannels());
-		final ArrayList<Integer> zList = parseList(zString, input.getNSlices());
-		final ArrayList<Integer> tList = parseList(tString, input.getNFrames());
+	public static ImagePlus makeSubhyperstack(ImagePlus input, String cString, String zString, String tString) {
+		ArrayList<Integer> cList = parseList(cString, input.getNChannels());
+		ArrayList<Integer> zList = parseList(zString, input.getNSlices());
+		ArrayList<Integer> tList = parseList(tString, input.getNFrames());
 		return makeSubhyperstack(input, cList, zList, tList);
 	}
 
-	public static ImagePlus makeSubhyperstack(final ImagePlus input,
-		final List<Integer> cList, final List<Integer> zList,
-		final List<Integer> tList)
-	{
+	public static ImagePlus makeSubhyperstack(ImagePlus input, List<Integer> cList, List<Integer> zList, List<Integer> tList) {
 		// validate inputs
-		if (cList.size() == 0) {
+		if (cList.size() == 0)
 			throw new IllegalArgumentException("Must specify at least one channel");
-		}
-		if (zList.size() == 0) {
+		if (zList.size() == 0)
 			throw new IllegalArgumentException("Must specify at least one slice");
-		}
-		if (tList.size() == 0) {
+		if (tList.size() == 0)
 			throw new IllegalArgumentException("Must specify at least one frame");
-		}
 
-		final ImageStack inputStack = input.getImageStack();
+		ImageStack inputStack = input.getImageStack();
 
-		final int cCount = input.getNChannels();
-		final int zCount = input.getNSlices();
-		final int tCount = input.getNFrames();
+		int cCount = input.getNChannels();
+		int zCount = input.getNSlices();
+		int tCount = input.getNFrames();
 
-		for (final int c : cList)
+		for (int c : cList)
 			check("C", c, cCount);
-		for (final int z : zList)
+		for (int z : zList)
 			check("Z", z, zCount);
-		for (final int t : tList)
+		for (int t : tList)
 			check("T", t, tCount);
 
 		// create output image
-		final String title = WindowManager.getUniqueName(input.getTitle());
-		ImagePlus output =
-			input.createHyperStack(title, cList.size(), zList.size(), tList.size(),
-				input.getBitDepth());
-		final ImageStack outputStack = output.getImageStack();
+		String title = WindowManager.getUniqueName(input.getTitle());
+		ImagePlus output = input.createHyperStack(title, cList.size(), zList.size(), tList.size(), input.getBitDepth());
+		ImageStack outputStack = output.getImageStack();
 
 		// add specified planes to subhyperstack
 		int oc = 0, oz, ot;
-		for (final int c : cList) {
+		for (int c : cList) {
 			oc++;
 			oz = 0;
-			for (final int z : zList) {
+			for (int z : zList) {
 				oz++;
 				ot = 0;
-				for (final int t : tList) {
+				for (int t : tList) {
 					ot++;
-					final int i = input.getStackIndex(c, z, t);
-					final int oi = output.getStackIndex(oc, oz, ot);
-					final String label = inputStack.getSliceLabel(i);
-					final ImageProcessor ip = inputStack.getProcessor(i);
+					int i = input.getStackIndex(c, z, t);
+					int oi = output.getStackIndex(oc, oz, ot);
+					String label = inputStack.getSliceLabel(i);
+					ImageProcessor ip = inputStack.getProcessor(i);
 					outputStack.setSliceLabel(label, oi);
 					outputStack.setPixels(ip.getPixels(), oi);
+					//IJ.log("  "+c + "  "+z+"  "+t+"  "+i +" "+oi+"  "+outputStack.getProcessor(1).getPixelValue(0,0));	
 				}
 			}
 		}
+		output.setStack(outputStack);
 
 		// propagate composite image settings, if appropriate
 		if (input instanceof CompositeImage) {
-			final CompositeImage compositeInput = (CompositeImage) input;
-			final CompositeImage compositeOutput =
+			CompositeImage compositeInput = (CompositeImage) input;
+			CompositeImage compositeOutput =
 				new CompositeImage(output, compositeInput.getMode());
 			oc = 0;
-			for (final int c : cList) {
+			for (int c : cList) {
 				oc++;
-				final LUT table = compositeInput.getChannelLut(c);
+				LUT table = compositeInput.getChannelLut(c);
 				compositeOutput.setChannelLut(table, oc);
 				compositeOutput.setPositionWithoutUpdate(oc, 1, 1);
 				compositeInput.setPositionWithoutUpdate(c, 1, 1);
-				final double min = compositeInput.getDisplayRangeMin();
-				final double max = compositeInput.getDisplayRangeMax();
+				double min = compositeInput.getDisplayRangeMin();
+				double max = compositeInput.getDisplayRangeMax();
 				compositeOutput.setDisplayRange(min, max);
 			}
 			output = compositeOutput;
 		}
-
-		// return result
 		return output;
 	}
 
-	private static void
-		check(final String name, final int index, final int count)
-	{
+	private static void check(String name, int index, int count) {
 		if (index < 1 || index > count) {
 			throw new IllegalArgumentException("Invalid " + name + " index: " +
 				index);
 		}
 	}
 
-	private static ArrayList<Integer> parseList(final String planeString,
-		int count)
-	{
-		final ArrayList<Integer> list = new ArrayList<Integer>();
-		for (final String token : planeString.split("\\s*,\\s*")) {
-			final int dash1 = token.indexOf("-");
-			final int dash2 = token.lastIndexOf("-");
+	private static ArrayList<Integer> parseList(String planeString, int count) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (String token : planeString.split("\\s*,\\s*")) {
+			int dash1 = token.indexOf("-");
+			int dash2 = token.lastIndexOf("-");
 			if (dash1 < 0) {
 				// single number
-				final int index;
+				int index;
 				try {
 					index = Integer.parseInt(token);
-				}
-				catch (final NumberFormatException exc) {
+				} catch (NumberFormatException exc) {
 					throw new IllegalArgumentException("Invalid number: " + token);
 				}
-				if (index < 1 || index > count) {
+				if (index < 1 || index > count)
 					throw new IllegalArgumentException("Invalid number: " + token);
-				}
 				list.add(Integer.parseInt(token));
-			}
-			else {
+			} else {
 				// range, with or without increment
-				final int min, max, step;
+				int min, max, step;
 				try {
 					min = Integer.parseInt(token.substring(0, dash1));
 					if (dash1 == dash2) {
 						// range (e.g. 2-14)
 						max = Integer.parseInt(token.substring(dash1 + 1));
 						step = 1;
-					}
-					else {
+					} else {
 						// range with increment (e.g. 1-100-2)
 						max = Integer.parseInt(token.substring(dash1 + 1, dash2));
 						step = Integer.parseInt(token.substring(dash2 + 1));
 					}
-				}
-				catch (final NumberFormatException exc) {
+				} catch (NumberFormatException exc) {
 					throw new IllegalArgumentException("Invalid range: " + token);
 				}
-				if (min < 1 || min > max || max > count || step < 1) {
+				if (min < 1 || min > max || max > count || step < 1)
 					throw new IllegalArgumentException("Invalid range: " + token);
-				}
-				for (int index = min; index <= max; index += step) {
+				for (int index = min; index <= max; index += step)
 					list.add(index);
-				}
 			}
 		}
 		return list;
