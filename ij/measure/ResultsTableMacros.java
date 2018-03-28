4f539c8a567fb42c78225c63abb205eb53c28970
Wayne Rasband <rasband@gmail.com>
2018-03-28 10:26:50 -0400

    2018.03.28 (1.52a23)

--- ij/measure/ResultsTableMacros.java
+++ ij/measure/ResultsTableMacros.java
@@ -21,14 +21,14 @@ public class ResultsTableMacros implements Runnable, DialogListener, ActionListe
 	private String title;
 	private int runCount;
 	private TextArea ta;
-	
+
 	public ResultsTableMacros(ResultsTable rt) {
 		this.rt = rt;
 		title = rt!=null?rt.getTitle():null;
 		Thread thread = new Thread(this, "ResultTableMacros");
 		thread.start();
 	}
-	
+
 	private void showDialog() {
 		if (rt==null)
 			rt = Analyzer.getResultsTable();
@@ -37,21 +37,22 @@ public class ResultsTableMacros implements Runnable, DialogListener, ActionListe
 			return;
 		}
 		String[] temp = rt.getHeadingsAsVariableNames();
-		String[] variableNames = new String[temp.length+1];
-		variableNames[0] = "";
-		for (int i=1; i<variableNames.length; i++)
-			variableNames[i] = temp[i-1];
+		String[] variableNames = new String[temp.length+2];
+		variableNames[0] = "Insert...";
+		variableNames[1] = "rowIndex";
+		for (int i=2; i<variableNames.length; i++)
+			variableNames[i] = temp[i-2];
 		String dialogTitle = "Apply Macro to "+(title!=null?"\""+title+"\"":"Table");
 		Frame parent = title!=null?WindowManager.getFrame(title):null;
 		if (parent!=null)
 			gd = new GenericDialog(dialogTitle, parent);
 		else
 			gd = new GenericDialog(dialogTitle);
+		gd.setInsets(5, 5, 0);
 		gd.addTextAreas(getMacro(), null, 12, 48);
 		ta = gd.getTextArea1();
 		ta.addKeyListener(this);
-		gd.addChoice("Insert:", variableNames, variableNames[0]);
-		
+
 		Panel panel = new Panel();
 		if (IJ.isMacOSX())
 			panel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
@@ -60,27 +61,29 @@ public class ResultsTableMacros implements Runnable, DialogListener, ActionListe
 		panel.add(runButton);
 		resetButton = new Button("Reset");
 		resetButton.addActionListener(this);
-		panel.add(resetButton);		
+		panel.add(resetButton);
 		openButton = new Button("Open");
 		openButton.addActionListener(this);
 		panel.add(openButton);
 		saveButton = new Button("Save");
 		saveButton.addActionListener(this);
 		panel.add(saveButton);
-		gd.addToSameRow();
 		gd.addPanel(panel);
+		gd.addToSameRow();
+		gd.addChoice("", variableNames, variableNames[0]);
+
 
 		gd.addHelp("<html><body><h1>Macro Equations for Results Tables</h1><ul>"+
 				"<li>The macro, or a selection, is applied to each row of the table."+
-				"<li>A new variable starting with an Uppercase character creates a new column."+
+				"<li>A new variable starting with an Uppercase character creates<br>a new column."+
 				"<li>A new variable starting with a lowercase character is temporary."+
 				"<li>The variable <b>rowIndex</b> is pre-defined.\n"+
-				"<li>String operations are supported for the 'Label' column only (if enabled<br>"+
+				"<li>String operations are supported for the 'Label' column only (if<br>enabled"+
 				"with Analyze&gt;Set Measurements&gt;Display Label)."+				
 				"<li>Click \"<b>Run</b>\" to apply the macro code to the table."+
 				"<li>Select a line and press "+(IJ.isMacOSX()?"cmd":"ctrl") + "-r to apply a line of macro code."+
 				"<li>Click \"<b>Reset</b>\" to revert to the original version of the table."+
-				"<li>The code is saved at <b>macros/TableMacro.ijm</b> when you click \"<b>OK</b>\"."+
+				"<li>The code is saved at <b>macros/TableMacro.ijm</b>, and the<br>\"Apply Macro\" command is recorded, when you click \"<b>OK</b>\"."+
 				"</ul></body></html>");
 
 		gd.addDialogListener(this);
@@ -91,7 +94,7 @@ public class ResultsTableMacros implements Runnable, DialogListener, ActionListe
 			return;
 		}
 		if (runCount==0)
-			applyMacro();			
+			applyMacro();
 		if (Recorder.record) {
 			String macro = getMacroCode();
 			macro = macro.replaceAll("\n", " ");
@@ -113,20 +116,26 @@ public class ResultsTableMacros implements Runnable, DialogListener, ActionListe
 		updateDisplay();
 		runCount++;
 	}
-	
+
 	private String getMacroCode() {
 		int start = ta.getSelectionStart();
 		int end = ta.getSelectionEnd();
 		return start==end?ta.getText():ta.getSelectedText();
 	}
-	
+
 	public boolean dialogItemChanged(GenericDialog gd, AWTEvent e) {
-		String variableName = gd.getNextChoice();
-		if (e!=null && (e.getSource() instanceof Choice) && variableName.length()>0) {
-			int pos = ta.getCaretPosition();
-			ta.insert(variableName, pos);
-			 ((Choice)e.getSource()).select(0);
-			//ta.setCaretPosition(pos+variableName.length());
+		final String variableName = gd.getNextChoice();
+		if (e!=null && (e.getSource() instanceof Choice) && !variableName.equals("Insert...")) {
+			final int pos = ta.getCaretPosition();
+			((Choice)e.getSource()).select(0);
+			final TextArea textArea = ta;
+			new Thread(new Runnable() {
+					public void run() {
+						IJ.wait(100);
+						textArea.insert(variableName, pos);
+						textArea.setCaretPosition(pos+variableName.length());
+						textArea.requestFocus();
+					}}).start();
 		}
 		return true;
 	}
@@ -155,8 +164,8 @@ public class ResultsTableMacros implements Runnable, DialogListener, ActionListe
 		}
 
 	}
-	 
-	public void keyPressed(KeyEvent e) { 
+
+	public void keyPressed(KeyEvent e) {
 		int flags = e.getModifiers();
 		boolean control = (flags & KeyEvent.CTRL_MASK) != 0;
 		boolean meta = (flags & KeyEvent.META_MASK) != 0;
@@ -167,32 +176,33 @@ public class ResultsTableMacros implements Runnable, DialogListener, ActionListe
 			rt = (ResultsTable)rtBackup.clone();
 			updateDisplay();
 		}
-	} 
-	
+	}
+
 	private void updateDisplay() {
 		if (title!=null)
 			rt.show(title);
 	}
-	
+
 	public void keyReleased(KeyEvent e) {
 	}
-	
+
 	public void keyTyped(KeyEvent e) {
 	}
-	 
+
 	private String getMacro() {
 		String macro = IJ.openAsString(IJ.getDir("macros")+NAME);
 		if (macro==null || macro.startsWith("Error:"))
 			return defaultMacro;
 		else {
-			macro =  macro.replaceAll("rowNumber", "rowIndex");
+			macro = macro.replaceAll("rowNumber", "rowIndex");
 			return macro;
 		}
 	}
-	 
+
 	public void run() {
 		rtBackup = (ResultsTable)rt.clone();
 		showDialog();
  	}
- 		 
+
 }
+
