ce25209f41e000e2d8fb5f2276a614047afc35dc
Curtis Rueden <ctrueden@wisc.edu>
2018-05-02 08:26:26 -0700

    Add Travis CI configuration

---
+++ .travis/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
+sh travis-build.sh $encrypted_69a1215a2d6a_key $encrypted_69a1215a2d6a_iv
---
+++ README.md
@@ -0,0 +1,2 @@
+[![](https://travis-ci.com/imagej/ij1-tests.svg?branch=master)](https://travis-ci.com/imagej/ij1-tests)
+
