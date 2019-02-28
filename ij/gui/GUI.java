392ba2dfcfa0dd15918d93ed79bce64faf623b11
Tiago Ferreira <tiago.tferr@gmail.com>
2019-02-28 16:36:25 -0500

    GUI: Add method for scaling Swing components
    (See description on GUI#scale(JComponent)

--- ij/gui/GUI.java
+++ ij/gui/GUI.java
@@ -1,5 +1,11 @@
 package ij.gui;
 import java.awt.*;
+
+import javax.swing.JComponent;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+
 import ij.*;
 
 /** This class consists of static GUI utility methods. */
@@ -63,6 +69,46 @@ public class GUI {
 		if (font == null) font = DEF_GUI_FONT;
 		font = font.deriveFont(scale * font.getSize());
 		component.setFont(font);
+	}
+
+	/**
+	 * Tries to detect if a Swing component is unscaled and scales it it according
+	 * to {@link #getGuiScale()}.
+	 * <p>
+	 * This is mainly relevant to linux: Swing components scale automatically on
+	 * most platforms, specially since Java 8. However there are still exceptions to
+	 * this on linux: e.g., In Ubuntu, Swing components do scale, but only under the
+	 * GTK L&F. (On the other hand AWT components do not scale <i>at all</i> on
+	 * hiDPI screens on linux).
+	 * </p>
+	 * <p>
+	 * This method tries to avoid exaggerated font sizes by detecting if a component
+	 * has been already scaled by the UIManager, applying only
+	 * {@link #getGuiScale()} to the component's font if not.
+	 * </p>
+	 *
+	 * @param component the component to be scaled
+	 * @return true, if component's font was resized
+	 */
+	public static boolean scale(final JComponent component) {
+		final double guiScale = Prefs.getGuiScale();
+		if (guiScale == 1d)
+			return false;
+		Font font = component.getFont();
+		if (font == null && component instanceof JList)
+			font = UIManager.getFont("List.font");
+		else if (font == null && component instanceof JTable)
+			font = UIManager.getFont("Table.font");
+		else if (font == null)
+			font = UIManager.getFont("Label.font");
+		if (font.getSize() > DEF_GUI_FONT.getSize())
+			return false;
+		if (component instanceof JTable)
+			((JTable) component).setRowHeight((int) (((JTable) component).getRowHeight() * guiScale * 0.9));
+		else if (component instanceof JList)
+			((JList<?>) component).setFixedCellHeight((int) (((JList<?>) component).getFixedCellHeight() * guiScale * 0.9));
+		component.setFont(font.deriveFont((float) guiScale * font.getSize()));
+		return true;
 	}
 
 	public static void scalePopupMenu(final PopupMenu popup) {
