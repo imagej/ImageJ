50cd75d70fbd6f6009cb54dd9eba3fd3cecc1e0a
Wayne Rasband <rasband@gmail.com>
2023-08-01 16:16:37 -0400

    2023.08.01 (1.54g9; Added functions.html to ij.jar)

--- ij/macro/FunctionFinder.java
+++ ij/macro/FunctionFinder.java
@@ -3,6 +3,7 @@ import ij.*;
 import ij.plugin.*;
 import ij.plugin.frame.*;
 import ij.gui.GUI;
+import ij.util.Tools;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Hashtable;
@@ -25,17 +26,11 @@ public class FunctionFinder implements TextListener,  WindowListener, KeyListene
 	public FunctionFinder(Editor editor) {
 
 		this.editor = editor;
-
-		String exists = IJ.runMacro("return File.exists(getDirectory('macros')+'functions.html');");
-		if (exists!=null && exists.equals("0"))	{
-			String installLocalMacroFunctionsFile = "functions = File.openUrlAsString(\""+url+"\");\n"+
-			"f = File.open(getDirectory('macros')+'functions.html');\n"+
-			"print (f, functions);\n"+
-			"File.close(f);";
-			try {IJ.runMacro(installLocalMacroFunctionsFile);
-			} catch (Throwable e) { IJ.error("Problem downloading functions.html"); return;}
+		String f = Tools.openFromIJJarAsString("/functions.html");
+		if (f==null) {
+			IJ.error("\"functions.html\" not found in ij.jar");
+			return;
 		}
-		String f = IJ.runMacro("return File.openAsString(getDirectory('macros')+'functions.html');");
 		f = f.replaceAll("&quot;", "\"");
 		String [] l = f.split("\n");
 		commands= new String [l.length];
