8fa08fa17d0729930209bb1cf894c72f7d63e6e3
Wayne Rasband <rasband@gmail.com>
2024-06-13 11:22:04 -0400

    2024.06.13 (1.54k1; FolderOpener)

--- ij/plugin/FolderOpener.java
+++ ij/plugin/FolderOpener.java
@@ -535,16 +535,17 @@ public class FolderOpener implements PlugIn, TextListener {
 				fi.longOffset = fi.getOffset() + i*(size + fi.getGap());
 				stack.addImage(fi);
 			}
-		} else {
-			FileInfo fi = info[0];
-			if (fi.fileType==FileInfo.RGB48) {
-				for (int slice=1; slice<=3; slice++) {
-					FileInfo fi2 = (FileInfo)fi.clone();
-					fi2.sliceNumber = slice;
-					stack.addImage(fi2);
-				}
-			} else
-				stack.addImage(fi);
+		} else {		
+			for (FileInfo fi : info) {
+				if (fi.fileType == FileInfo.RGB48) {
+					for (int slice = 1; slice <= 3; slice++) {
+						FileInfo fi2 = (FileInfo) fi.clone();
+						fi2.sliceNumber = slice;
+						stack.addImage(fi2);
+					}
+				} else
+					stack.addImage(fi);
+			}
 		}
 	}
 	
